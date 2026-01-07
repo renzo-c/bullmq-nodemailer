9d0fc423578c1f684ca85bc3ca55b4072e21ffa6
Renzo Navarro <rcnavarrop@gmail.com>
2026-01-06 23:01:22 -0500

    add jaeger for trace storage and visualization

--- src/instrumentation/instrumentation.consumer.ts
+++ src/instrumentation/instrumentation.consumer.ts
@@ -1,16 +1,22 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { PeriodicExportingMetricReader, ConsoleMetricExporter } from '@opentelemetry/sdk-metrics';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+
 
 const sdk = new NodeSDK({
   serviceName: 'consumer',
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+  }),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4318/v1/metrics',
+    }),
   }),
   instrumentations: [
     new ExpressInstrumentation(),
--- src/instrumentation/instrumentation.producer.ts
+++ src/instrumentation/instrumentation.producer.ts
@@ -1,16 +1,21 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { PeriodicExportingMetricReader, ConsoleMetricExporter } from '@opentelemetry/sdk-metrics';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { IORedisInstrumentation } from '@opentelemetry/instrumentation-ioredis';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 
 const sdk = new NodeSDK({
   serviceName: 'producer',
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+  }),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4318/v1/metrics',
+    }),
   }),
   instrumentations: [
     new ExpressInstrumentation(),
